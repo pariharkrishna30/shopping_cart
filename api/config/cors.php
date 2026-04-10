b035cdabe806c888a78992d124ffdd856368a745
Krishna Parihar <krishna.parihar@quickshift.in>
2026-04-10 12:15:44 +0530

    added docker setup for full project

--- api/config/cors.php
+++ api/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => explode(',', env('FRONTEND_URLS', 'http://localhost:5173,http://127.0.0.1:5173')),
+    'allowed_origins' => explode(',', env('FRONTEND_URLS', 'http://localhost:3000,http://127.0.0.1:3000')),
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
